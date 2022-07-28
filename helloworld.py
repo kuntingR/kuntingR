f7980d61c02ad7b499e7eb1e1af6b7e5c2f1ea27
kunting <1263242411@qq.com>
2022-07-28 23:19:26 +0800

    test for hello file

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,15 @@
-print("hello world!")
+import time
+
+
+class HelloWorld():
+    def __init__(self):
+        pass
+
+    def print_hello(self, input_time):
+        for t in range(input_time):
+            print("the {} time for hello world !".format(t+1))
+            time.sleep(1)
+
+
+hello = HelloWorld()
+hello.print_hello(10)
